4fe5c442f13b57251fcaac50e6e1570d374fd401
DPEDITS <debashishparida75@gmail.com>
2024-08-19 23:28:44 +0530

    modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,9 +46,9 @@ function Navbarweb() {
           </Nav>
           <span className='navbar-text'>
             <div className='social-icon'>
-                <a href='#'><img src={navIcon1} alt=""/></a>
-                <a href='#'><img src={navIcon2} alt=""/></a>
-                <a href='#'><img src={navIcon3} alt=""/></a>
+                <a href='#'><img src={navIcon1} alt="Linkedin"/></a>
+                <a href='#'><img src={navIcon2} alt="FaceBook"/></a>
+                <a href='instagram.com/dp_edits29'><img src={navIcon3} alt="Instagram"/></a>
             </div>
             <button className='vvd' onClick={()=>console.log('connect')}><span>Let's Connect</span></button>
           </span>
