7c38142f280cb8d9646f6810a7747cc1a751e88e
DPEDITS <debashishparida75@gmail.com>
2024-08-20 21:26:26 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Projects from './components/Projects'
 import { Banner } from './components/Banner';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Contact from './components/Contact';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
     <Banner/>
     <Skills/>
     <Projects/>
+    <Contact/>
     </>
   )
 }
