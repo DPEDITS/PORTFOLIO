ae2973cfe4a6b4855134876664738be57a6bfe5c
DPEDITS <debashishparida75@gmail.com>
2025-07-22 23:30:26 +0530

    added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useState,useEffect } from 'react';
-import logo from '../assets/img/logo.svg' 
+import logo from '../../public/logo.png' 
 import navIcon1 from '../assets/img/nav-icon1.svg' 
 import navIcon2 from '../assets/img/nav-icon2.svg' 
 import navIcon3 from '../assets/img/nav-icon3.svg' 
@@ -33,7 +33,7 @@ function Navbarweb() {
     <Navbar expand="lg" className={scrolled ?"scrolled": ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={logo} alt="LOGO"/>
+            <img src={logo} alt="LOGO" className="navbar-logo"/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className='navbar-toggler-icon'></span>
